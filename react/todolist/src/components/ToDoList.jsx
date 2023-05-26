22fc093ca8370671bd6ce3c3c8a4b2ab1a7d65fa
kyungkyuBae <qorudrb14@naver.com>
2023-05-26 18:40:20 +0900

    ALL Reset
    기존에 컴포넌트의 분리는
    규칙있는 분리가 아니라
    올 리셋하고 다시 만들어보기로 함

--- react/todolist/src/components/ToDoList.jsx
+++ react/todolist/src/components/ToDoList.jsx
@@ -1,17 +1,10 @@
-import React from 'react';
-import Main from './Main';
-import Header from './Header';
-import AddTodo from './AddTodo';
+!import React from 'react';
 
-
-export default function ToDoList() {
+export default function todolist() {
     return (
-        <section className="w-96 h-96 bg-cyan-300 flex-col">
-            <Header />
-            <Main />
-            <AddTodo />
+        <div>
             
-        </section>
+        </div>
     );
 }
 
